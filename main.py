f09f5196365fe05ad25fb62526141518a3902476
AlexSim199513 <alex199513@gmail.com>
2024-06-01 17:24:10 -0400

    finished report functionality

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_letters = get_num_letters(text)
-    print(f"{num_words} words found in the document")
-    print(num_letters)
+    num_letters_string = return_num_letters(num_letters)
+    print("Book Report for Mary Shelley's Frankenstein")
+    print(f"{num_words} words found in the document \n")
+    print(num_letters_string)
+    print("End of Report")
 
 
 def get_num_words(text):
@@ -35,6 +38,12 @@ def get_num_letters(text):
 
     return letter_count
 
-
+def return_num_letters(letters):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    strings = []
+    for char in alphabet:
+       if char in letters:
+        strings.append(f"The '{char}' character was found {letters[char]} times")
+    return "\n".join(strings)
 
 main()
